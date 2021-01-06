1e645dc4140d2b438ae386ef96ceef09e50a8426
isaworld2016 <isaworld2016@gmail.com>
2021-01-06 18:08:06 +0900

    switchbox add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,6 +16,7 @@ import {
 } from '@ionic/react';
 import { home, personCircle } from 'ionicons/icons';
 import { default as React } from 'react';
+import './Home.scss';
 interface IEvent {
   eventNm?: string;
   date?: string;
@@ -27,30 +28,30 @@ interface IApvLIst {
   checked?: boolean;
 }
 export const Home: React.FC = () => {
-  const asdf:IEvent[] = [
-    { eventNm: '신년행사', date: "2020-01-01"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
-    { eventNm: '상철이생일', date: "2020-01-26"},
+  const asdf: IEvent[] = [
+    { eventNm: '신년행사', date: "2020-01-01" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
   ]
-  const apvList:IApvLIst[] = [
-    {name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01"},   
-    {name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01"},   
-    {name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01"},   
-    {name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01"},   
-    {name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01"},   
-    {name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01"},   
+  const apvList: IApvLIst[] = [
+    { name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01" },
+    { name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01" },
+    { name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01" },
+    { name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01" },
+    { name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01" },
+    { name: '이지혜', checked: false, email: "isaworld@naver.com", reqDate: "2021-01-01" },
   ]
   return (
     <IonPage>
@@ -72,13 +73,28 @@ export const Home: React.FC = () => {
       </IonHeader>
       <IonContent>
         <div className='px-container'>
-          <div style={{marginTop: "20px"}} className="apv-wrap">
+          {/* 알리엑스 스위치 박스 가져옴 */}
+          <div className="switch-box">
+            <label>
+              <input type="radio" name="rdo" checked />
+              <span>One</span>
+            </label>
+            <label>
+              <input type="radio" name="rdo" />
+              <span>Two</span>
+            </label>
+            <label>
+              <input type="radio" name="rdo" />
+              <span>Three</span>
+            </label>
+          </div>
+          <div style={{ marginTop: "20px" }} className="apv-wrap">
             <header>
-              <h4 style={{ display: "inline"}}><strong>가입승인을 기다려요</strong></h4>
+              <h4 style={{ display: "inline" }}><strong>가입승인을 기다려요</strong></h4>
               <span> {apvList.length} / 100</span>
-              <IonButton style={{ marginLeft:"60px", transform: "translateY(-10px)" }} size="small" color="dark">승인</IonButton>
+              <IonButton style={{ marginLeft: "60px", transform: "translateY(-10px)" }} size="small" color="dark">승인</IonButton>
             </header>
-            <div className="apv-list-wrap" style={{marginLeft:"-5px"}}>
+            <div className="apv-list-wrap" style={{ marginLeft: "-5px" }}>
               <IonGrid>
                 <IonRow>
                   <IonCol size="1"></IonCol>
@@ -86,12 +102,12 @@ export const Home: React.FC = () => {
                   <IonCol size="5">이메일</IonCol>
                   <IonCol size="4">가입신청일</IonCol>
                 </IonRow>
-                {apvList.map((a, i) =>(
+                {apvList.map((a, i) => (
                   <IonRow key={i}>
-                    <IonCol size="1" style={{maxWidth: "28px", width:"28px"}}><IonCheckbox style={{width:"23px", height:"23px"}} checked={a.checked} color="dark"/></IonCol>
-                    <IonCol size="2" style={{fontSize:"13px"}}>{a.name}</IonCol>
-                    <IonCol size="5" style={{fontSize:"13px"}}>{a.email}</IonCol>
-                    <IonCol size="4" style={{fontSize:"13px"}}>{a.reqDate}</IonCol>
+                    <IonCol size="1" style={{ maxWidth: "28px", width: "28px" }}><IonCheckbox style={{ width: "23px", height: "23px" }} checked={a.checked} color="dark" /></IonCol>
+                    <IonCol size="2" style={{ fontSize: "13px" }}>{a.name}</IonCol>
+                    <IonCol size="5" style={{ fontSize: "13px" }}>{a.email}</IonCol>
+                    <IonCol size="4" style={{ fontSize: "13px" }}>{a.reqDate}</IonCol>
                   </IonRow>
                 ))}
               </IonGrid>
@@ -99,15 +115,15 @@ export const Home: React.FC = () => {
           </div>
           <br />
           <div className="month-event-wrap">
-            <header style={{ marginBottom: "-30px"}}>
+            <header style={{ marginBottom: "-30px" }}>
               <h4>
-                <strong style={{ display: "inline-block"}}>
+                <strong style={{ display: "inline-block" }}>
                   이번달 우리마을 일정
                 </strong>
               </h4>
             </header>
-            <br/>
-              {/* <IonList>
+            <br />
+            {/* <IonList>
                 {asdf.map((v, i) => (
                   <IonItem key={i}>
                     <IonLabel>
@@ -116,26 +132,26 @@ export const Home: React.FC = () => {
                   </IonItem>
                 ))}
               </IonList> */}
-              <div className="month-event-list-wrap" style={{marginLeft: "-5px"}}>
-                <IonList>
-                  {asdf.map((v, i) => (
-                    <IonCard key={i} style={{marginBottom: "-10px"}}>
-                      <IonCardHeader>
-                        {/* <IonCardSubtitle>{v.date}</IonCardSubtitle> */}
-                        <IonCardTitle style={{fontSize: "14px"}}>
-                          {v.date} &nbsp;&nbsp;&nbsp;&nbsp;{v.eventNm}
-                        </IonCardTitle>
-                      </IonCardHeader>
+            <div className="month-event-list-wrap" style={{ marginLeft: "-5px" }}>
+              <IonList>
+                {asdf.map((v, i) => (
+                  <IonCard key={i} style={{ marginBottom: "-10px" }}>
+                    <IonCardHeader>
+                      {/* <IonCardSubtitle>{v.date}</IonCardSubtitle> */}
+                      <IonCardTitle style={{ fontSize: "14px" }}>
+                        {v.date} &nbsp;&nbsp;&nbsp;&nbsp;{v.eventNm}
+                      </IonCardTitle>
+                    </IonCardHeader>
 
-                      {/* <IonCardContent>
+                    {/* <IonCardContent>
                         Keep close to Nature's heart... and break clear away, once in awhile,
                         and climb a mountain or spend a week in the woods. Wash your spirit clean.
                       { v.eventNm }
                       </IonCardContent> */}
-                    </IonCard>
-                  ))}
-                </IonList>
-              </div>
+                  </IonCard>
+                ))}
+              </IonList>
+            </div>
           </div>
           {/* <NewsSearch />
           <NewsList /> */}
