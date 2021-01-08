9d90820ce99238a579d8632e731d78fa87e5fb63
isaworld2016 <isaworld2016@gmail.com>
2021-01-08 18:20:34 +0900

    commit

--- src/pages/Example.tsx
+++ src/pages/Example.tsx
@@ -9,9 +9,29 @@ import {
 } from '@ionic/react';
 import React from 'react';
 import { PageTopHomeAndMyPageBtn } from '../components/organisms/PageTopHomeAndMyPageBtn';
-import './Home.scoped.scss';
-
+import './Example.scoped.scss';
+interface IEvent {
+  eventNm?: string;
+  date?: string;
+}
 export const Example: React.FC = () => {
+  const asdf: IEvent[] = [
+    { eventNm: '신년행사', date: "2020-01-01" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+    { eventNm: '상철이생일', date: "2020-01-26" },
+  ]
   return (
     <>
       <IonPage>
@@ -25,7 +45,7 @@ export const Example: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          {/* 알리엑스 스위치 박스 가져옴 */}
+          {/* 스위치 박스 */}
           <div className="switch-box">
             <label>
               <input type="radio" name="rdo" checked />
@@ -39,6 +59,24 @@ export const Example: React.FC = () => {
               <input type="radio" name="rdo" />
               <span>Three</span>
             </label>
+          </div>
+          {/* 컬러 라벨 */}
+          <div className="top-desc">
+            <span className="yellow">warning</span>
+            <span className="red">critical</span>
+          </div>
+          {/* 리스트 */}
+          <div className="list-box">
+            <ul>
+              {asdf.map((v, i) => (
+                <li key={i}>
+                  <strong className="tit">{v.eventNm}</strong>
+                  <p className="txt">
+                    <span>{v.date} {v.eventNm}</span>
+                  </p>
+                </li>
+              ))}
+            </ul>
           </div>
         </IonContent>
       </IonPage>
