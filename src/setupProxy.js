b1522d17d86726794bf4a191c42ea004cc9ac34b
origin <isaworld2016@gmail.com>
2021-07-27 23:16:52 +0900

    feat: 프록시 설정2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,3 @@
-// 작성일 기준 프록시 설정하는 방법이 아래와 같이 변경되었습니다.
-// 최신 방법이므로 사용하는데 문제가 없습니다.
-
-// setupProxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
