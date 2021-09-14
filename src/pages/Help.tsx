631694b6cc41da8f3bd34a8da1dbfccfc122a202
origin <isaworld2016@gmail.com>
2021-09-14 23:25:44 +0900

    help page 수정

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,7 +1,9 @@
 import {
   IonContent,
+  IonIcon,
   IonPage,
 } from '@ionic/react'
+import { desktop, desktopOutline, logoGithub, logoGoogle, logoGooglePlaystore } from 'ionicons/icons'
 import React from 'react'
 import { PageHeader } from '../components/molecules/PageHeaderComponent'
 
@@ -10,9 +12,26 @@ export const Help: React.FC = () => {
   return (
     <IonPage>
       <PageHeader pageTitle="Help" menuBtn={false} closeLeftBtn={true} homeBtn={true} userBtn={true}/>
-      <IonContent>
-        <div className='px-container my-4' style={{textAlign:'center'}}>
-          mocomocolaps@gmail.com
+      <br/>
+      <IonContent className='text-center'>
+        <IonIcon icon={logoGoogle} className='mr5'></IonIcon>
+        <div className='px-container my-4'>
+          mocomocolabs@gmail.com
+        </div>
+
+        <IonIcon icon={logoGithub} className='mr5'></IonIcon>
+        <div className='px-container my-4'>
+          https://github.com/mocomocolabs
+        </div>
+
+        <IonIcon icon={logoGooglePlaystore} className='mr5'></IonIcon>
+        <div className='px-container my-4'>
+          https://play.google.com/store/apps/details?id=com.mocomoco.hama
+        </div>
+
+        <IonIcon icon={desktopOutline} className='mr5'></IonIcon>
+        <div className='px-container my-4'>
+          https://hama-world.netlify.app
         </div>
       </IonContent>
     </IonPage>
